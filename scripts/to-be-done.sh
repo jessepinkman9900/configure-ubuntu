7d84c1d7482e1006957d0717a0c323a6a7feb573
kishansairam9 <kishansairam.adapa9@gmail.com>
2019-01-14 23:47:59 +0530

    Updated

--- scripts/to-be-done.sh
+++ scripts/to-be-done.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Themes are copied, Enable using Gnome-Tweak-Tools"
-echo "Run these commands to enable OneDrive on start : systemctl --user enable onedrive && systemctl --user start onedrive"
-echo "Visit https://extensions.gnome.org/extension/1162/emoji-selector/ and install emoji extension"
+echo "1) Themes are copied, Enable using Gnome-Tweak-Tools"
+echo
+echo "2) Run these commands to enable OneDrive on start:"
+echo "  	systemctl --user enable onedrive"
+echo "  	systemctl --user start onedrive"
+echo 
+echo "   Visit https://github.com/skilion/onedrive to know about onedrive client"
+echo
+echo "3) Visit https://extensions.gnome.org/extension/1162/emoji-selector/ and install emoji extension"
+echo
+echo "4) Gestures are installed and should work perfectly fine once you restart/ logout & login back."
+echo
+echo "The gestures configuration is"
+tail scripts/multitouch-gestures.sh
+echo
