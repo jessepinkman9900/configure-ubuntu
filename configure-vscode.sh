52bd344f66c38cec3e6eef177609545c18c7187c
Kishan Sairam Adapa <kishansairam.adapa9@gmail.com>
2019-01-18 20:35:05 +0530

    Update configure-vscode.sh

--- configure-vscode.sh
+++ configure-vscode.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 echo "Listing VSCode extensions as configure works only after initial run"
-code --list-extensions # Might be buggy, might not work.
+#code --list-extensions # Might be buggy, might not work.
 
 # Install Extensions
 
 while read -r line || [[ -n "$line" ]];do
 	code --install-extension $line	
-done < ../VSCode-Config/vscode-extensions.txt
+done < ../VSCode-Config/vscode-extensions
 
 # Change settings.json to preffered settings
 
