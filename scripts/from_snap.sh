8941e32fff2fb3a9dad271d33426c44199978c6e
Kishan Sairam Adapa <kishansairam.adapa9@gmail.com>
2019-01-18 08:40:39 +0530

    Removed brave(Buggy), added chromium

--- scripts/from_snap.sh
+++ scripts/from_snap.sh
@@ -12,7 +12,7 @@ snap install ao # Microsoft To-Do Client
 mkdir -p ~/.config/autostart
 cp /snap/ao/current/meta/gui/ao.desktop ~/.config/autostart
 snap install termius-app
-snap install brave
+snap install chromium
 snap install vlc
 snap install gimp
 snap install hugo
